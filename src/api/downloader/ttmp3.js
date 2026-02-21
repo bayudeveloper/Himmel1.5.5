87cdee46eeecdc7d73014e3c653206e23e132a8c
bayudeveloper <anugrahabayu4@gmail.com>
2026-02-21 22:32:32 +0700

    Update ttmp3.js

--- src/api/downloader/ttmp3.js
+++ src/api/downloader/ttmp3.js
@@ -1,77 +1,42 @@
-const https = require("https");
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+const axios = require("axios");
 
 module.exports = function(app) {
-    function apiRequest(url) {
-        return new Promise((resolve, reject) => {
-            const protocol = url.startsWith("https") ? https : http;
-
-            protocol.get(url, (res) => {
-                let data = "";
-                res.on("data", chunk => data += chunk);
-                res.on("end", () => {
-                    try {
-                        resolve(JSON.parse(data));
-                    } catch {
-                        resolve(data);
-                    }
-                });
-            }).on("error", reject);
-        });
-    }
-
-    function downloadFile(url, filepath) {
-        return new Promise((resolve, reject) => {
-            const protocol = url.startsWith("https") ? https : http;
-            const file = fs.createWriteStream(filepath);
-
-            protocol.get(url, (response) => {
-
-                if (response.statusCode === 301 || response.statusCode === 302) {
-                    return downloadFile(response.headers.location, filepath)
-                        .then(resolve)
-                        .catch(reject);
+    async function tiktokDownload(url) {
+        try {
+            const response = await axios.post('https://www.tikwm.com/api/', {
+                url: url,
+                count: 12,
+                cursor: 0,
+                web: 1,
+                hd: 1
+            }, {
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    'User-Agent': 'Mozilla/5.0'
                 }
-
-                response.pipe(file);
-
-                file.on("finish", () => {
-                    file.close();
-                    resolve(filepath);
-                });
-
-            }).on("error", (err) => {
-                fs.unlink(filepath, () => {});
-                reject(err);
             });
-        });
-    }
 
-    async function getTikWM(url) {
-        const apiUrl = `https://www.tikwm.com/api/?url=${encodeURIComponent(url)}`;
-        const response = await apiRequest(apiUrl);
-
-        if (response.code !== 0 || !response.data) {
-            throw new Error("Gagal ambil data TikWM");
-        }
-
-        const data = response.data;
-
-        return {
-            title: data.title,
-            author: data.author?.unique_id,
-            duration: data.duration,
-            audio: `https://www.tikwm.com${data.music}`,
-            cover: data.cover,
-            stats: {
-                likes: data.digg_count,
-                comments: data.comment_count,
-                shares: data.share_count,
-                plays: data.play_count
+            if (response.data.code === 0) {
+                const data = response.data.data;
+                return {
+                    title: data.title,
+                    author: data.author.unique_id,
+                    audio: data.music ? `https://www.tikwm.com${data.music}` : null,
+                    cover: data.cover,
+                    duration: data.duration,
+                    stats: {
+                        play: data.play_count,
+                        like: data.digg_count,
+                        comment: data.comment_count,
+                        share: data.share_count
+                    }
+                };
             }
-        };
+            throw new Error('Failed to get data');
+        } catch (err) {
+            throw err;
+        }
     }
 
     app.get("/downloader/ttmp3", async (req, res) => {
@@ -85,25 +50,12 @@ module.exports = function(app) {
         }
 
         try {
-            const data = await getTikWM(url);
-
-            const filename = `${data.author}_${Date.now()}.mp3`;
-            const downloadDir = path.join(__dirname, "../../../downloads");
-
-            if (!fs.existsSync(downloadDir)) {
-                fs.mkdirSync(downloadDir, { recursive: true });
-            }
-
-            const filepath = path.join(downloadDir, filename);
-
-            await downloadFile(data.audio, filepath);
-
+            const result = await tiktokDownload(url);
+            
             res.json({
                 status: true,
-                info: data,
-                download: `/downloads/${filename}`
+                data: result
             });
-
         } catch (err) {
             res.status(500).json({
                 status: false,

48132b8178c021eee5b488f6623d7665e6f61c4d
bayudeveloper <anugrahabayu4@gmail.com>
2026-02-22 03:39:00 +0700

    Update ytmp4.js

--- src/api/downloader/ytmp4.js
+++ src/api/downloader/ytmp4.js
@@ -1,4 +1,4 @@
-const ytdl = require("ytdl-core");
+const ytdl = require('ytdl-core');
 
 module.exports = function(app) {
     app.get("/downloader/ytmp4", async (req, res) => {
@@ -12,6 +12,7 @@ module.exports = function(app) {
         }
 
         try {
+            // Validasi URL
             if (!ytdl.validateURL(url)) {
                 return res.status(400).json({
                     status: false,
@@ -19,9 +20,21 @@ module.exports = function(app) {
                 });
             }
 
-            const info = await ytdl.getInfo(url);
-            const format = ytdl.chooseFormat(info.formats, { quality: 'highestvideo' });
-            
+            // Dapatkan info video
+            const info = await ytdl.getInfo(url, {
+                requestOptions: {
+                    headers: {
+                        'User-Agent': 'Mozilla/5.0'
+                    }
+                }
+            });
+
+            // Pilih format video dengan audio
+            const format = ytdl.chooseFormat(info.formats, { 
+                quality: 'highestvideo',
+                filter: 'audioandvideo'
+            });
+
             res.json({
                 status: true,
                 data: {
@@ -29,10 +42,11 @@ module.exports = function(app) {
                     author: info.videoDetails.author.name,
                     duration: info.videoDetails.lengthSeconds,
                     thumbnail: info.videoDetails.thumbnails[info.videoDetails.thumbnails.length - 1].url,
-                    video: format.url,
-                    quality: format.qualityLabel || 'Unknown'
+                    video_url: format.url,
+                    quality: format.qualityLabel
                 }
             });
+
         } catch (err) {
             res.status(500).json({
                 status: false,

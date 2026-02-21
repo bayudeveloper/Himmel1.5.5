102bb569e818a956821cf3c23d6902dba9506b24
bayudeveloper <anugrahabayu4@gmail.com>
2026-02-21 17:22:58 +0700

    Create ytmp3.js

---
+++ src/api/downloader/ytmp3.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const ytdl = require("ytdl-core");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const PORT = 3002;
+
+app.get("/yt/mp3", async (req, res) => {
+  const url = req.query.url;
+
+  if (!url) {
+    return res.status(400).json({
+      status: false,
+      message: "Masukkan parameter ?url="
+    });
+  }
+
+  if (!ytdl.validateURL(url)) {
+    return res.status(400).json({
+      status: false,
+      message: "URL tidak valid"
+    });
+  }
+
+  try {
+    const info = await ytdl.getInfo(url);
+
+    const title = info.videoDetails.title.replace(/[^a-z0-9]/gi, "_").toLowerCase();
+    const filename = `${title}.mp3`;
+    const filepath = path.join(__dirname, filename);
+
+    const metadata = {
+      title: info.videoDetails.title,
+      author: info.videoDetails.author.name,
+      duration: info.videoDetails.lengthSeconds,
+      views: info.videoDetails.viewCount,
+      thumbnail: info.videoDetails.thumbnails.pop().url
+    };
+
+    ytdl(url, { filter: "audioonly", quality: "highestaudio" })
+      .pipe(fs.createWriteStream(filepath))
+      .on("finish", () => {
+        res.json({
+          status: true,
+          info: metadata,
+          download: `http://localhost:${PORT}/${filename}`
+        });
+      });
+
+    app.use(express.static(__dirname));
+
+  } catch (err) {
+    res.status(500).json({
+      status: false,
+      error: err.message
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 MP3 API jalan di http://localhost:${PORT}`);
+});

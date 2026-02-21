cbd53348c89b13ebc047c24a8ba7041741c8b413
bayudeveloper <anugrahabayu4@gmail.com>
2026-02-21 18:07:01 +0700

    Create ffinfo.js

---
+++ src/api/info(berita)/ffinfo.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const app = express();
+const PORT = 3002;
+
+app.get("/ffinfo", async (req, res) => {
+  const { type, id } = req.query;
+
+  if (!type || !id)
+    return res.json({ status: false, message: "Masukkan ?type= & ?id=" });
+
+  try {
+    let hasil;
+
+    if (type === "character") {
+      const response = await axios.get(`https://ff.garena.com/id/chars/${id}`);
+      const $ = cheerio.load(response.data);
+
+      hasil = {
+        title: $(".skill-profile-title").text().trim(),
+        name: $(".skill-profile-name").text().trim(),
+        skill: $(".skill-introduction").text().trim(),
+      };
+    }
+
+    else if (type === "pet") {
+      const response = await axios.get(`https://ff.garena.com/id/pets/${id}`);
+      const $ = cheerio.load(response.data);
+
+      hasil = {
+        name: $(".skill-profile-name").text().trim(),
+        skill: $(".skill-introduction").text().trim(),
+      };
+    }
+
+    else {
+      return res.json({ status: false, message: "Type tidak valid" });
+    }
+
+    res.json({ status: true, data: hasil });
+
+  } catch (err) {
+    res.status(500).json({ status: false, message: err.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`🔥 FF INFO API jalan di http://localhost:${PORT}`);
+});

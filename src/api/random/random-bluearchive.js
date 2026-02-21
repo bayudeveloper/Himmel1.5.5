00ee7357e20ac9fe6f0fbd028c08205209d101f8
bayudeveloper <anugrahabayu4@gmail.com>
2026-02-21 19:25:52 +0700

    Update random-bluearchive.js

--- src/api/random/random-bluearchive.js
+++ src/api/random/random-bluearchive.js
@@ -1,14 +1,18 @@
 const axios = require('axios');
+
 module.exports = function(app) {
     async function bluearchive() {
         try {
-            const { data } = await axios.get(`https://raw.githubusercontent.com/rynxzyy/blue-archive-r-img/refs/heads/main/links.json`)
-            const response = await axios.get(data[Math.floor(data.length * Math.random())], { responseType: 'arraybuffer' });
+            const { data } = await axios.get(`https://raw.githubusercontent.com/rynxzyy/blue-archive-r-img/refs/heads/main/links.json`);
+            const response = await axios.get(data[Math.floor(data.length * Math.random())], { 
+                responseType: 'arraybuffer' 
+            });
             return Buffer.from(response.data);
         } catch (error) {
             throw error;
         }
     }
+
     app.get('/random/ba', async (req, res) => {
         try {
             const pedo = await bluearchive();
